a3cd5b12efd03599d77d82c6db8b98cd340040b7
jeremy orion <jeremy9315@hotmail.fr>
2017-04-13 10:40:07 +0200

    Ajoutez des fichiers projet.

---
+++ Exo7/Donnees.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice6
+{
+    /// <summary>
+    /// classe générale des données à gérer
+    /// -----------------------------------
+    /// classe qui regroupe les données
+    /// de l'application de gestion des stagiaires
+    /// dans une collection d'objets définie en static (=non instanciable)
+    /// </summary>
+    public class Donnees
+    {
+        /// <summary>
+        /// collection d'objets MStagiaire
+        /// </summary>
+        public static List<MStagiaire>ArrayStag = new List<MStagiaire>();
+    }
+}
